44c75f4c0d07f96d66a1413574518c7750c9c441
Andre Felipe <kodec_andrade@hotmail.com>
2020-06-21 00:48:24 -0400

    Instalação de 2 Pacotes - EnityFrameworkCore.SqlServer.Design e EntityFrameworkCore.Tools para funcionar as migrations.

---
+++ WebApi-Core/WebApi-Core/Migrations/20200621044553_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi_Core.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Preco = table.Column<double>(nullable: false),
+                    Descricao = table.Column<string>(nullable: true),
+                    TipoProduto = table.Column<string>(nullable: true),
+                    DataCadastro = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ProdutoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
