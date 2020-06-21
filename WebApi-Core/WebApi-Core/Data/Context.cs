1ff5d6e5df2398f1a8ad0a872fb623e5d6a5e809
Andre Felipe <kodec_andrade@hotmail.com>
2020-06-20 23:53:54 -0400

    Criação da primeira parte do acesso ao banco com DbContext.

---
+++ WebApi-Core/WebApi-Core/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Core.Models;
+
+namespace WebApi_Core.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) {}
+        
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
