6d336555482cf712982aa3a508499f04ccf678e3
Andre Felipe <kodec_andrade@hotmail.com>
2020-06-21 00:35:26 -0400

    Install-Package EntityFramework

--- WebApi-Core/WebApi-Core/Models/Produto.cs
+++ WebApi-Core/WebApi-Core/Models/Produto.cs
@@ -12,6 +12,6 @@ namespace WebApi_Core.Models
         public double Preco { get; set; }
         public string Descricao { get; set; }
         public string TipoProduto { get; set; }
-        public string DataCadastro { get; set; }
+        public DateTime DataCadastro { get; set; }
     }
 }
